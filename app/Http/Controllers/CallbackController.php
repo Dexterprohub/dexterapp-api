95817d2990b2897f1eb338780649b5b432c7eac1
Olatayo Olayemi <olatayo.olayemi.peter@gmail.com>
2023-06-09 00:49:36 +0100

    wip

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Iamolayemi\Paystack\Paystack;
+use App\Enums\CheckoutStatus;
+use App\Enums\PaymentStatus;
+use App\Models\Checkout;
+use Paystack;
 use Illuminate\Http\Request;
 
 class CallbackController extends Controller
@@ -15,14 +18,32 @@ class CallbackController extends Controller
             abort(404);
         }
 
+        $checkout = Checkout::where('order_number', $reference)->firstOrFail();
+
+        // if the checkout is already completed
+        if($checkout->payment_status == PaymentStatus::SUCCESSFUL) {
+           return 'Payment has already been made for this order';
+        }
+
         $payment = Paystack::transaction()->verify($reference)->response('data');
 
         // check payment status
         if ($payment['status'] == 'success') {
-            // payment is successful
-            // code your business logic here
+            // update checkout
+            $checkout->update([
+                'status' => CheckoutStatus::COMPLETED,
+                'payment_status' => PaymentStatus::SUCCESSFUL,
+            ]);
+
+            // redirect to success page
+            return 'Payment has been made successfully';
         } else {
-            // payment is not successful
+            $checkout->update([
+                'status' => CheckoutStatus::CANCELLED,
+                'payment_status' => PaymentStatus::FAILED,
+            ]);
+
+           return'Payment failed';
         }
 
     }
