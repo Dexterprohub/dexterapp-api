0cacfac978e61c6370cfda9e623bb89566254bad
Olatayo Olayemi <olatayo.olayemi.peter@gmail.com>
2023-06-09 01:26:53 +0100

    wip

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Enums\CheckoutStatus;
 use App\Enums\PaymentStatus;
 use App\Models\Checkout;
-use Paystack;
 use Illuminate\Http\Request;
+use Paystack;
 
 class CallbackController extends Controller
 {
@@ -21,30 +21,31 @@ class CallbackController extends Controller
         $checkout = Checkout::where('order_number', $reference)->firstOrFail();
 
         // if the checkout is already completed
-        if($checkout->payment_status == PaymentStatus::SUCCESSFUL) {
-           return 'Payment has already been made for this order';
+        if ($checkout->payment_status == PaymentStatus::SUCCESSFUL || $checkout->status == CheckoutStatus::COMPLETED) {
+            return 'Payment has already been made for this order';
         }
 
         $payment = Paystack::transaction()->verify($reference)->response('data');
 
-        // check payment status
+        // payment is successful
         if ($payment['status'] == 'success') {
             // update checkout
             $checkout->update([
-                'status' => CheckoutStatus::COMPLETED,
                 'payment_status' => PaymentStatus::SUCCESSFUL,
             ]);
 
-            // redirect to success page
-            return 'Payment has been made successfully';
-        } else {
-            $checkout->update([
-                'status' => CheckoutStatus::CANCELLED,
-                'payment_status' => PaymentStatus::FAILED,
-            ]);
-
-           return'Payment failed';
+            // return success message
+            return 'Payment has been made successfully and awaiting vendor confirmation';
         }
+
+        // payment failed
+        $checkout->update([
+            'status' => CheckoutStatus::CANCELLED,
+            'payment_status' => PaymentStatus::FAILED,
+        ]);
+
+        // return error message
+        return 'Payment failed';
 
     }
 }
